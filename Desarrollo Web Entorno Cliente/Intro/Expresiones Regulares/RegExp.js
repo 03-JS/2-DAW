26b838cd8c63ea3dc39e4308ba603b7edb3c0361
03-JS <pyjoso3vitaslim@gmail.com>
2024-09-17 13:57:38 +0200

    Regex exercises

---
+++ Desarrollo Web Entorno Cliente/Intro/Expresiones Regulares/RegExp.js
@@ -0,0 +1,22 @@
+/*
+1
+*/
+
+
+
+/*
+2
+*/
+
+var mystring = 'Una cadena. De prueba.';
+var newString = mystring.replace(/\./g, "_");
+
+console.log(newString);
+
+/*
+3
+*/
+
+var str = "Una cadena de prueba";
+var regex = /a/g;
+str.match(regex);
