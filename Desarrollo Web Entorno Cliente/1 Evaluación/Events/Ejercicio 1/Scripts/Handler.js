265f99ca4fcdfd0ef4fb6c6803d8f5c3be857d3b
03-JS <pyjoso3vitaslim@gmail.com>
2024-10-04 10:14:29 +0200

    Update Handler.js

--- Desarrollo Web Entorno Cliente/1 Evaluación/Events/Ejercicio 1/Scripts/Handler.js
+++ Desarrollo Web Entorno Cliente/1 Evaluación/Events/Ejercicio 1/Scripts/Handler.js
@@ -11,12 +11,8 @@ let errStr = "";
 nameInput.onblur = () => { if (/^\d+$/.test(nameInput.value)) errStr += "El nombre no puede tener carácteres numéricos\n"; }
 surInput.onblur = () => { if (/^\d+$/.test(surInput.value)) errStr += "Los apellidos no pueden tener carácteres numéricos\n"; }
 emailInput.onblur = () => {
-    if (!emailInput.value.includes("@")) {
-        errStr += "El correo ha de incluir un '@'\n";
-    }
-    if (!emailInput.value.includes(".")) {
-        errStr += "El correo ha de incluir un '.'\n";
-    }
+    if (!emailInput.value.includes("@")) errStr += "El correo ha de incluir un '@'\n";
+    if (!emailInput.value.includes(".")) errStr += "El correo ha de incluir un '.'\n";
 }
 dniInput.onblur = () => {
     if (emailInput.value.length != 9 || !/^[A-Za-z]+$/.test(emailInput.value.charAt(emailInput.value.length - 1))) {
@@ -24,24 +20,16 @@ dniInput.onblur = () => {
     }
 }
 passInput.onblur = () => {
-    if (passInput.value.length != 8) {
-        errStr += "La contraseña ha de tener un mínimo de 8 carácteres\n";
-    }
-    if (!/^[A-Za-z]+$/.test(passInput.value)) {
-        errStr += "La contraseña ha de contener una letra mayúscula y una minúscula\n";
-    }
-    if (!/^\d+$/.test(passInput.value)) {
-        errStr += "La contraseña ha de contener un número\n";
-    }
+    if (passInput.value.length != 8) errStr += "La contraseña ha de tener un mínimo de 8 carácteres\n";
+    if (!/^[A-Za-z]+$/.test(passInput.value)) errStr += "La contraseña ha de contener una letra mayúscula y una minúscula\n";
+    if (!/^\d+$/.test(passInput.value)) errStr += "La contraseña ha de contener un número\n";
 }
 rePassInput.onblur = () => {
-    if (rePassInput.value != passInput.value) {
-        errStr += "La contraseña no es la misma\n";
-    }
+    if (rePassInput.value != passInput.value) errStr += "La contraseña no es la misma\n";
 }
 ipInput.onblur = () => {
     if (!/(?:(?:25[0-5]|2[0-4]\d|[01]?\d?\d{1})\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d?\d{1})/g.test(ipInput.value)) {
-        errStr += "IP no valida\n";
+        errStr += "IP no válida\n";
     }
 }
 
