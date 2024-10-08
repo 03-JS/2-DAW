43c2e88c2796999fc73943868437cb6ae3724568
03-JS <pyjoso3vitaslim@gmail.com>
2024-10-08 12:21:46 +0200

    Shapes exercise done

---
+++ Desarrollo Web Entorno Cliente/1 Evaluación/DOM/Scripts/Logic.js
@@ -0,0 +1,28 @@
+function ChangeShape(element) {
+    element.classList.add("circle");
+}
+
+function RestoreShape(element) {
+    if (!element.classList.contains("interior-shadow")) element.classList.remove("circle");
+}
+
+function AddShadow(element) {
+    element.classList.add("shadowed");
+}
+
+function RemoveShadow(element) {
+    element.classList.remove("shadowed");
+}
+
+function TransformShape(element) {
+    element.classList.add("circle");
+    element.classList.add("interior-shadow");
+    element.classList.remove("shadowed");
+}
+
+function DeleteElements(elements) {
+    for (let i = 0; i < elements.length; i++) {
+        let element = document.querySelector(elements[i]);
+        element.remove();
+    }
+}
