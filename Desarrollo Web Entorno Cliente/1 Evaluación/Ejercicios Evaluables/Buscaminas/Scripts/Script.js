eb6f65bc87f6a9a5045bc4ab400365430f608f4c
JS03 <pyjoso3vitaslim@gmail.com>
2024-10-25 13:34:52 +0200

    Minesweeper progress

--- Desarrollo Web Entorno Cliente/1 Evaluación/Ejercicios Evaluables/Buscaminas/Scripts/Script.js
+++ Desarrollo Web Entorno Cliente/1 Evaluación/Ejercicios Evaluables/Buscaminas/Scripts/Script.js
@@ -1,19 +1,22 @@
 let points = 0;
 
 function Main() {
+    // Slots click event
     document.addEventListener("click", (event) => {
         if (event.target.classList.contains("casilla") && event.target.classList.contains("oculto")) {
             event.target.classList.remove("oculto");
-            points += eval(`(${event.target.innerText} + 1) * ${numMinas.value}`);
+            if (event.target.classList.contains("poco") || event.target.classList.contains("medio") || event.target.classList.contains("mucho")) points += eval(`(${event.target.innerText} + 1) * ${numMinas.value}`);
             puntos.innerHTML = points + `<br>puntos`;
         }
         if (event.target.classList.contains("mina")) {
-            mensajeFinal.innerText = `Has perdido con ${points} puntos`;
+            let textNode = Array.from(mensajeFinal.childNodes).find(node => node.nodeType === Node.TEXT_NODE);
+            textNode.textContent = `Has perdido con ${points} puntos`;
             protector.classList.remove("ocultar");
             mensajeFinal.classList.remove("ocultar");
         };
     });
 
+    // Other click events
     empezar.addEventListener("click", () => {
         if (numMinas.value < 5 || numMinas.value > 50) error.innerText = "Tiene que ser un valor entre 5 y 50";
         else {
@@ -23,12 +26,27 @@ function Main() {
             CreateSlots();
         }
     });
+    
+    volver.addEventListener("click", () => {
+        location.reload();
+    });
 }
 
 function CreateSlots() {
     for (let i = 0; i < 100; i++) {
         let slot = document.createElement("div");
+        slot.textContent = `${i}`;
         slot.classList.add("casilla", "oculto");
         tablero.appendChild(slot);
+    }
+    let i = 0;
+    let rand = 0;
+    let slotNodes = document.querySelectorAll(".casilla");
+    while (i != numMinas.value) {
+        do {
+            rand = Math.floor(Math.random() * slotNodes.length);
+            slotNodes.item(Array.prototype.indexOf.call(slotNodes, slotNodes[rand])).classList.add("mina");
+        } while (!slotNodes[rand].classList.contains("mina"));
+        i++;
     }
 }
