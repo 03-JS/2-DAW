37a626a0c87dc08b2e4ec530bb8fe4063d567d9d
03-JS <pyjoso3vitaslim@gmail.com>
2024-12-02 14:57:38 +0100

    Slight home page redesign

---
+++ Diseño de Interfaces/1 Evaluación/Ejercicios Evaluables/Cliente/Scripts/common.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+    logo.addEventListener("click", () => {window.location.replace("./home.html");});
+    profileIcon.addEventListener("click", () => {window.location.replace("./profile.html");});
+});
