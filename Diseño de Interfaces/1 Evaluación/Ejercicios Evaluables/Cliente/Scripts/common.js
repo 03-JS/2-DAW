7428f0eec995ecc153042252cbe0a4d526195cee
03-JS <pyjoso3vitaslim@gmail.com>
2024-12-08 20:19:39 +0100

    Website finished

--- Diseño de Interfaces/1 Evaluación/Ejercicios Evaluables/Cliente/Scripts/common.js
+++ Diseño de Interfaces/1 Evaluación/Ejercicios Evaluables/Cliente/Scripts/common.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-    logo.addEventListener("click", () => {window.location.replace("./home.html");});
-    profileIcon.addEventListener("click", () => {window.location.replace("./profile.html");});
+    logo.addEventListener("click", () => window.location.replace("./home.html"));
+    profileIcon.addEventListener("click", () => window.location.replace("./profile.html"));
+    document.querySelectorAll(".item").forEach(element => element.addEventListener("click", () => {window.location.replace("./car_preview.html")}));
+    buy.addEventListener("click", () => window.location.replace("./purchase.html"));
 });
