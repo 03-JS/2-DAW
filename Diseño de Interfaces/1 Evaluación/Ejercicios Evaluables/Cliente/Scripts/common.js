6e5a3e1e7e4aac51aaf2e64af8ce7ff282934737
03-JS <pyjoso3vitaslim@gmail.com>
2024-12-09 09:42:24 +0100

    Small change

--- Diseño de Interfaces/1 Evaluación/Ejercicios Evaluables/Cliente/Scripts/common.js
+++ Diseño de Interfaces/1 Evaluación/Ejercicios Evaluables/Cliente/Scripts/common.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-    logo.addEventListener("click", () => window.location.replace("./home.html"));
-    profileIcon.addEventListener("click", () => window.location.replace("./profile.html"));
-    document.querySelectorAll(".item").forEach(element => element.addEventListener("click", () => {window.location.replace("./car_preview.html")}));
-    buy.addEventListener("click", () => window.location.replace("./purchase.html"));
+    logo.addEventListener("click", () => window.location.href = "./home.html");
+    profileIcon.addEventListener("click", () => window.location.href = "./profile.html");
+    document.querySelectorAll(".item").forEach(element => element.addEventListener("click", () => window.location.href = "./car_preview.html"));
+    buy.addEventListener("click", () => window.location.href = "./purchase.html");
 });
