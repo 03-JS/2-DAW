79f51b01759c215d60b646f1321ec2445f07a350
JS03 <pyjoso3vitaslim@gmail.com>
2024-12-19 17:57:16 +0100

    Now uses venv

--- Desarollo Web Entorno Servidor/php/T4_PHP/E1_APLICWEBENPHP_ALUMNO/Scripts/ProcessForm.php
+++ Desarollo Web Entorno Servidor/php/T4_PHP/E1_APLICWEBENPHP_ALUMNO/Scripts/ProcessForm.php
@@ -1,9 +1,11 @@
 <?php
 
-$model = $_POST["currentModel"];
-$prompt = $_POST["prompt"];
+// $model = $_POST["currentModel"];
+// $prompt = $_POST["prompt"];
+$model = "microsoft/Phi-3.5-mini-instruct";
+$prompt = "Escribe los números primos del 1 al 100";
 
-$command = escapeshellcmd("python ./AI.py " . escapeshellarg($model) . " " . escapeshellarg($prompt));
+$command = escapeshellcmd(escapeshellarg("../../../../../../2-DAW/.venv/Scripts/python.exe ") . "./AI.py " . escapeshellarg($model) . " " . escapeshellarg($prompt));
 $output = shell_exec($command);
 $output = mb_convert_encoding($output, 'UTF-8', mb_detect_encoding($output, 'UTF-8, ISO-8859-1', true));
 
